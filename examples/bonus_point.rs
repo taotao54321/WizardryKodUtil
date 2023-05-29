316ff9d55ea63feb9f8cb34bd2ee754efb6bda06
taotao54321 <taotao54321@gmail.com>
2023-05-30 00:33:05 +0900

    add: bonus_point

---
+++ examples/bonus_point.rs
@@ -0,0 +1,33 @@
+use wizardry_kod_util::*;
+
+fn main() {
+    solve();
+}
+
+fn solve() {
+    let mut counts = [0; 7];
+
+    for state in 0..=u16::MAX {
+        let rng = Rng::from_state(state);
+        let extra = extra_bonus_count(rng);
+        counts[extra] += 1;
+    }
+
+    for (extra, &count) in counts.iter().enumerate() {
+        let ratio = f64::from(count) / f64::from(0x10000);
+        println!("+{}\t{count}/65536\t{ratio:.05}", 10 * extra);
+    }
+}
+
+fn extra_bonus_count(mut rng: Rng) -> usize {
+    let mut count = 0;
+
+    while rng.gen_range(20) == 15 {
+        count += 1;
+        if count >= 6 {
+            break;
+        }
+    }
+
+    count
+}
